b1277d2bfb4d3d3a93eb742243b964ccae68fd25
unknown <brian@simationstudios.com>
2025-11-10 10:33:14 +0500

    splash screen updated

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainStack from './src/navigation/stackNavigation';
+import AnimatedSplash from './src/components/SplashScreen/splashScreen';
 
 export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  if (showSplash) {
+    return <AnimatedSplash onFinish={() => setShowSplash(false)} />;
+  }
+
   return (
     <NavigationContainer>
       <MainStack />
---
+++ src/components/SplashScreen/splashScreen.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import LottieView from 'lottie-react-native';
+import SplashScreen from 'react-native-splash-screen';
+import { sizes } from '../../constants/sizes';
+
+interface AnimatedSplashProps {
+  onFinish: () => void;
+}
+
+const AnimatedSplash: React.FC<AnimatedSplashProps> = ({ onFinish }) => {
+  useEffect(() => {
+    SplashScreen.hide();
+
+    const timer = setTimeout(() => {
+      onFinish();
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, [onFinish]);
+
+  return (
+    <View style={styles.container}>
+      <LottieView
+        source={require('../../assets/animations/animatedSplashScreen.json')}
+        autoPlay
+        loop={false}
+        style={styles.lottie}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#FDCDDB',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  lottie: {
+    height: sizes.screenHeight,
+    width: sizes.screenWidth,
+  },
+});
+
+export default AnimatedSplash;
+
