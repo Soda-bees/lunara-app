632402f437f8d4bd14b7478bf55e81d8fbb49e13
SimationStudios <devfuzzz@gmail.com>
2025-11-21 15:44:52 +0500

    signin with apple integrated

---
+++ ios/Lunara-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Lunara-Bridging-Header.h
+//  Lunara
+//
+//  Created by jahan dost on 21/11/2025.
+//
+#import <GoogleSignIn/GoogleSignIn.h>
+
+#ifndef Lunara_Bridging_Header_h
+#define Lunara_Bridging_Header_h
+
+
+#endif /* Lunara_Bridging_Header_h */
